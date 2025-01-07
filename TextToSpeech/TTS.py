9d4f270b6e621cdb858e38a79683c6439e1ef74e
Bishal-cs <bishaldas1152006@gmail.com>
2025-01-07 20:54:59 +0530

    add

--- TextToSpeech/TTS.py
+++ TextToSpeech/TTS.py
@@ -1,10 +1,14 @@
-import pyttsx3                                                  #For text to speech download "pip install pyttsx3"
-
+import subprocess
+try:
+    import pyttsx3                                                  #For text to speech download "pip install pyttsx3"
+except ModuleNotFoundError:
+    subprocess.run("pip install pyttsx3")
 
 def speak(text):
     engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('voice', voices[0].id)
     engine.say(text)
     engine.runAndWait()
 
