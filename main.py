3cc3ed3ec0e51b60a6ba6d010c0714e5cc7aa672
Bishal-cs <bishaldas1152006@gmail.com>
2025-03-22 19:53:19 +0530

    fix main.py

--- main.py
+++ main.py
@@ -33,37 +33,4 @@ def main():
             Text()
         else:
             print("Invalid input")
-
 main()
-
-
-# def trancationg_input():
-#     with open("Data/input.txt","w") as file:
-#         file.truncate(0)
-
-# def check_inputs():
-#     output_text = ""
-#     while True:
-#         with open("Data/input.txt","r") as file:
-#             input_text = file.read().lower() 
-#         if input_text != output_text:
-#             output_text = input_text
-#             if "edit" in output_text or "hey edit" in output_text: 
-#                 response = ChatBot(output_text)
-#                 print("AI:",response)
-#                 speak(response)
-#                 trancationg_input()
-#             else:
-#                 pass
-#         else:
-#             pass
-
-# def main():
-#     t1 = threading.Thread(target = check_inputs)
-#     t2 = threading.Thread(target = listen)
-#     t1.start()
-#     t2.start()
-#     t1.join()
-#     t2.join()
-
-# main()
